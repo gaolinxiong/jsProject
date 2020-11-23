78568385c27719c8bbae9919858c5e77f8193068
gaolinxiong <1906972240@qq.com>
2020-11-23 15:42:02 +0800

    feat: update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <p @click="handleName">高林雄</p>
+      {{todoList}}
+      {{onShowList}}
+    </div>
+</template>
+<script>
+  export default ({
+    name: 'app',
+    data() {
+      return {
+        todoList: [],
+        onShowList: [123]
+      }
+    },
+    methods: {
+      handleName() {
+        console.log('高林雄')
+      }
+    }
+  })
+</script>
+
+<style>
+    @import 'assets/index.less';
+</style>
